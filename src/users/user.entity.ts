66c762e02b8a8671ddb5f9fa20eb4e4ff16ce403
LiRezende <librusamolin@gmail.com>
2025-05-26 20:20:34 -0300

    Users controller, repository and service.

---
+++ src/users/user.entity.ts
@@ -0,0 +1,26 @@
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    email: string;
+    @Column()
+    password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with Id', this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User with Id', this.id);
+    }
+
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with Id', this.id);
+    }
+}
